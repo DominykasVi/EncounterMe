fcb5cf2930232dac660173dab3e182b37a40bfd2
EmilijaSvirsk <emilijasvirsk@gmail.com>
2021-09-07 15:05:46 +0300

    created properly Hello World console project

---
+++ EncounterMe/EncounterMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EncounterMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
