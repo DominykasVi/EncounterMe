3b515c3455d08f0435be2083d5e4a042e55a4b9b
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2021-09-16 19:55:38 +0300

    add class Location with fields and functions

--- EncounterMe/EncounterMe/Program.cs
+++ EncounterMe/EncounterMe/Program.cs
@@ -6,7 +6,12 @@ namespace EncounterMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Team!");
+            Console.WriteLine("Hello World!");
+            Location location1 = new Location(001, "VU MIF Naugardukas", 54.67518129701089, 25.273545582365784);
+            Location location2 = new Location(002, "VU MIF Baltupiai", 54.729775633971855, 25.263535399566603);
+            Console.WriteLine(location1.Latitude);
+            Console.WriteLine(location1.Name);
+            Console.WriteLine(location1.distanceToUser(location2.Latitude, location2.Longtitude));
         }
     }
 }
