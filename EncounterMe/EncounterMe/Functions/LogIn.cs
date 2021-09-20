02eaa9dcea45217cd03705a43e266f0ffabb959b
AdamBeresnev <adam.beresnev@gmail.com>
2021-09-20 17:08:08 +0300

    Some changes for readability

--- EncounterMe/EncounterMe/Functions/LogIn.cs
+++ EncounterMe/EncounterMe/Functions/LogIn.cs
@@ -7,12 +7,15 @@ namespace EncounterMe.Functions
 {
     class LogIn
     {
-        public bool CheckPassword(string username, string password)             //Class for checking if the password is correct
+        public bool CheckPassword(string username, string password)             
+            //Class for checking if the password is correct
         {
-            return true;                                                         //Not sure how we're gonna store user cridentials, so didn't do this yet
+            return true;                                                         
+            //Not sure how we're gonna store user cridentials, so didn't do this yet
         }
 
-        public temp_User CreateUser (string username, string password)         //Change temp_User to User class when it is created
+        public temp_User CreateUser (string username, string password)         
+            //Change temp_User to User class when it is created
         {
             if (ExistingUsername(username) && ValidPassword(password))
                 return new temp_User(username, password);
@@ -20,9 +23,11 @@ namespace EncounterMe.Functions
                 return null;
         }
 
-        public bool ExistingUsername(string input)                  //Class that should check if the entered username already exists or not
+        public bool ExistingUsername(string input)                  
+            //Class that should check if the entered username already exists or not
         {
-            return false;                                        //Not sure how we're gonna store user cridentials, so didn't do this yet
+            return false;                                        
+            //Not sure how we're gonna store user cridentials, so didn't do this yet
         }
 
         public bool ValidPassword(string input)
@@ -31,7 +36,8 @@ namespace EncounterMe.Functions
             Regex hasLowercase = new Regex("[a-z]");
             Regex hasUppercase = new Regex("[A-Z]");
 
-            return hasNumbers.IsMatch(input) && hasLowercase.IsMatch(input) && hasUppercase.IsMatch(input);                                     //Currently doesn't check for other characters, couldn't figure it out
+            return hasNumbers.IsMatch(input) && hasLowercase.IsMatch(input) && hasUppercase.IsMatch(input);
+            //Currently doesn't check for other characters, couldn't figure it out
         }
     }
 }
