6db8193ca9b3b1e0c55295179ae8d0ee8ea8aa69
Dominykas <48029929+DominykasVi@users.noreply.github.com>
2021-09-20 20:56:07 +0300

    Added simple verification by name, that the record is new. Added TODO comments to db manager class.

--- EncounterMe/EncounterMe/Program.cs
+++ EncounterMe/EncounterMe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;//
+using EncounterMe.Functions;
 
 namespace EncounterMe
 {
@@ -7,29 +8,13 @@ namespace EncounterMe
     {
         static void Main(string[] args)
         {
-            Test_SaveLocation();
-        }
-
-        static void Test_SaveLocation()
-        {
-            Location location1 = new Location(001, "VU MIF Naugardukas", 54.67518129701089, 25.273545582365784);
-            Location location2 = new Location(002, "VU MIF Baltupiai", 54.729775633971855, 25.263535399566603);
-
-            var locationList = new List<Location>() { location1, location2 };
-            DatabaseManager db = new DatabaseManager("Test");
-
-            db.writeToFile(locationList);
-            var list = db.readSavedLocations();
-            //The Database Manager does not check what locations we already have in the file and keeps adding new ones
-
-            foreach (var value in list)
-            {
-                Console.WriteLine(value.Name);
-            }
-
-            //Console.WriteLine(location1.Latitude);
-            //Console.WriteLine(location1.Name);
-            //Console.WriteLine(location1.distanceToUser(location2.Latitude, location2.Longtitude));
+            //Test Code, non important
+            //Location location1 = new Location(001, "VU MIF Naugardukas", 54.67518129701089, 25.273545582365784);
+            //Location location2 = new Location(002, "VU MIF Baltupiai", 54.729775633971855, 25.263535399566603);
+            //Location location3 = new Location(003, "Test", 54.729775633971855, 25.263535399566603);
+            //var locationList = new List<Location>() { location1, location2, location3 };
+            //DatabaseManager db = new DatabaseManager("Test");
+            //db.writeToFile(locationList);
         }
 
     }
