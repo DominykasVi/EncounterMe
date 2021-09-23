2f68c3b2b4fa387dafa15eee314bcb78ada8ca9a
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2021-09-23 16:55:16 +0300

    change from double precision to float precision

--- EncounterMe/EncounterMe/Classes/Location.cs
+++ EncounterMe/EncounterMe/Classes/Location.cs
@@ -6,25 +6,26 @@ namespace EncounterMe
 {
     class Location
     {
+        private const float circumference = (float) 6372.795477598;
         public int ID { get; set; }
         public String Name { get; set; }
         //Coordiantes are to be written in Decimal Degree (DD) notation. See more here: https://en.wikipedia.org/wiki/Decimal_degrees
         //Might not require duoble precision, can be switched to float later
-        public double Latitude { get; set; }
-        public double Longtitude { get; set; }
+        public float Latitude { get; set; }
+        public float Longtitude { get; set; }
 
         //public Location(int id, String name, double northCoord, double eastCoord)
         public Location(int ID, String Name, double Latitude, double Longtitude)
         {
             this.ID = ID;
             this.Name = Name;
-            this.Latitude = Latitude;
-            this.Longtitude = Longtitude;
+            this.Latitude = (float) Latitude;
+            this.Longtitude = (float) Longtitude;
         }
 
-        public double distanceToUser(double lat, double lon)
+        public float distanceToUser(double lat, double lon)
         {
-            return 6372.795477598 * Math.Acos(Math.Sin(this.Latitude * Math.PI / 180.00) * Math.Sin(lat * Math.PI / 180.00) + Math.Cos(this.Latitude * Math.PI / 180.00) * Math.Cos(lat * Math.PI / 180.00) * Math.Cos((this.Longtitude - lon) * Math.PI / 180));
+            return (float) (circumference * Math.Acos(Math.Sin(this.Latitude * Math.PI / 180.00) * Math.Sin(lat * Math.PI / 180.00) + Math.Cos(this.Latitude * Math.PI / 180.00) * Math.Cos(lat * Math.PI / 180.00) * Math.Cos((this.Longtitude - lon) * Math.PI / 180)));
         }
 
     }
--- EncounterMe/EncounterMe/Program.cs
+++ EncounterMe/EncounterMe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-//using System.Collections.Generic;//
+using System.Collections.Generic;
+//using System.Collections.Generic;
 using EncounterMe.Functions;
 
 namespace EncounterMe
@@ -9,12 +10,13 @@ namespace EncounterMe
         static void Main(string[] args)
         {
             //Test Code, non important
-            //Location location1 = new Location(001, "VU MIF Naugardukas", 54.67518129701089, 25.273545582365784);
-            //Location location2 = new Location(002, "VU MIF Baltupiai", 54.729775633971855, 25.263535399566603);
-            //Location location3 = new Location(003, "Test", 54.729775633971855, 25.263535399566603);
-            //var locationList = new List<Location>() { location1, location2, location3 };
-            //DatabaseManager db = new DatabaseManager("Test");
-            //db.writeToFile(locationList);
+           /* Location location1 = new Location(001, "VU MIF Naugardukas", 54.67518129701089, 25.273545582365784);
+            Location location2 = new Location(002, "VU MIF Baltupiai", 54.729775633971855, 25.263535399566603);
+            Location location3 = new Location(003, "Test", 54.729775633971855, 25.263535399566603);
+            var locationList = new List<Location>() { location1, location2, location3 };
+            DatabaseManager db = new DatabaseManager("Test");
+            db.writeToFile(locationList);
+            Console.WriteLine(location1.distanceToUser(54.729775633971855, 25.263535399566603));*/
         }
 
     }
