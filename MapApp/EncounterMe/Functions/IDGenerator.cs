792d0d63fe9135e334a391df91e06b21770c16a8
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2021-09-30 15:52:28 +0300

    add class IDGenerator for creating location IDs

---
+++ MapApp/EncounterMe/Functions/IDGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncounterMe.Functions
+{
+    class IDGenerator
+    {
+        //lazy, thread-safe singleton pattern, based on https://csharpindepth.com/articles/singleton
+        private static readonly Lazy<IDGenerator> lazy =
+        new Lazy<IDGenerator>(() => new IDGenerator());
+
+        public static IDGenerator Instance { get { return lazy.Value; } }
+
+        private IDGenerator()
+        {
+        }
+
+        private int id = 0;
+
+        private bool idIsSet = false;
+
+        public void setID (IEnumerable<Location> loc)
+        {
+            foreach (Location location in loc)
+            {
+                if (location.ID > id) id = location.ID;
+            }
+            idIsSet = true;
+        }
+
+        public int getID ()
+        {
+            if (idIsSet == true)
+            {
+                id++;
+                return id;
+            }
+            else
+            {
+                throw new Exception("ID is not set!");
+            }
+        }
+    }
+}
