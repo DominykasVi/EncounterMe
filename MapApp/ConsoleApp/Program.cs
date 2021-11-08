323d1f1bf27b6d9a82a258e6f05156ce5cc3f079
AdamBeresnev <adam.beresnev@gmail.com>
2021-11-08 18:00:00 +0200

    Error fix in Program.cs

--- MapApp/ConsoleApp/Program.cs
+++ MapApp/ConsoleApp/Program.cs
@@ -26,44 +26,6 @@ namespace ConsoleApp
 
         //}
 
-        static void Test_UserCreate()
-        {
-            DatabaseManager db = new DatabaseManager("", "users");
-            string name;
-            string password;
-            string email;
-            Console.WriteLine("Name:");
-            name = Console.ReadLine();
-            Console.WriteLine("Email:");
-            email = Console.ReadLine();
-            Console.WriteLine("Password:");
-            password = Console.ReadLine();
 
-            LogInManager login = new LogInManager(db);
-            
-
-            User user = login.CreateUser(name, email, password);
-
-            Console.ReadLine();
-
-        }
-        
-        static void Test_UserLogIn()
-        {
-            DatabaseManager db = new DatabaseManager("", "users");
-            string name;
-            string password;
-            Console.WriteLine("Name:");
-            name = Console.ReadLine();
-            Console.WriteLine("Password:");
-            password = Console.ReadLine();
-
-            LogInManager login = new LogInManager(db);
-
-            User user = login.CheckPassword(name, password);
-
-            Console.ReadLine();
-
-        }
     }
 }
