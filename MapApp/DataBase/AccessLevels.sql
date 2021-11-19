6d9300d836247e250cf6510f7828edc161677de8
AdamBeresnev <adam.beresnev@gmail.com>
2021-11-19 02:42:34 +0200

    Removed Database Project

---
+++ MapApp/DataBase/AccessLevels.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].AccessLevel
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Permission name] NVARCHAR(50) NOT NULL
+)
---
+++ MapApp/DataBase/AccountUpvotes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].AccountUpvotes
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [AccountId] INT NOT NULL, 
+    [LocationId] INT NOT NULL,
+    CONSTRAINT FK_AccountUpvotes_Account FOREIGN KEY (AccountId) REFERENCES Account(Id),
+    CONSTRAINT FK_AccountUpvotes_Location FOREIGN KEY (LocationId) REFERENCES Location(Id)
+)
